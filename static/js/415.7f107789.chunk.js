0691afed981a5596f1842d52eee6ff9e8146d5e1
Nikita1825 <119980710+Nikita1825@users.noreply.github.com>
2023-12-15 16:16:08 +0000

    Deploying to gh-pages from @ Nikita1825/rent-car@0ecd99d015cf12567d9d4d598677d8c756660724 🚀

---
+++ static/js/415.7f107789.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t);r(791);var c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:"\u0430\u0440\u0442\u0441\u043f\u0440\u043e"})})}}}]);
+//# sourceMappingURL=415.7f107789.chunk.js.map
---
+++ static/js/643.c5e2b270.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[643],{643:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"Favourite"})})}}}]);
+//# sourceMappingURL=643.c5e2b270.chunk.js.map
